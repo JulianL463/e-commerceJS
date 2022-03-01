4bc55ad168acd04b44f037b1aec961bf4ab5599c
JulianL463 <julian.lopez.2818@gmail.com>
2022-03-01 17:09:36 -0300

    fetch

--- js/script.js
+++ js/script.js
@@ -1,14 +1,15 @@
 
-///////////////////////////////////////////Variables/////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+let stock = [];
 
-const stock =  [{id:'chimuelo', dispo:10, precio: 1500},
-                {id:'guante', dispo:10, precio: 700},
-                {id:'pikachu', dispo:10, precio: 250},
-                {id:'snitch', dispo:10, precio: 300},
-                {id:'groot', dispo:10, precio: 1000},
-                {id:'ironman', dispo:10, precio: 500},
-                {id:'kiloren', dispo:10, precio: 450}];
 
+//   [{id:'chimuelo', dispo:10, precio: 1500},
+//                 {id:'guante', dispo:10, precio: 700},
+//                 {id:'pikachu', dispo:10, precio: 250},
+//                 {id:'snitch', dispo:10, precio: 300},
+//                 {id:'groot', dispo:10, precio: 1000},
+//                 {id:'ironman', dispo:10, precio: 500},
+//                 {id:'kiloren', dispo:10, precio: 450}];
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////
 let carrito = new Carrito();
 let carritoCant = document.getElementById('carritoCount');
@@ -16,35 +17,41 @@ let carritoCant = document.getElementById('carritoCount');
 
 carrito.cargarCarrito();
 
+fetch('js/productos.json')
+    .then((resp) => resp.json())
+    .then((data) => {
+        stock = data;
+        console.log(data);
+        console.log(stock);
+        stock.forEach((prod)=>{
 
-stock.forEach((prod)=>{
-
-    const {id, precio, dispo} = prod;
-    let item = document.createElement("div");
-
-    item.className = "producto";
-    item.innerHTML = `<div class="producto__titulo"><h3>${id.toUpperCase()}</h3></div>
-                    <div class="producto__img"><img src="img/${id}.jpg" alt=""></div>
-                    <div class="producto__info">
-                        <h4 class="producto__info__precio">$${precio}</h4>
-                        <input id="${id}AddBtn" class="producto__info__boton" type="button" value="">
-                     </div>;`;
-
-    document.getElementById('productos__grid').appendChild(item);
-
-    let addBtn = document.getElementById(id+'AddBtn');
-    addBtn.onclick = () => {
-        carrito.agregarProducto(new Producto(id, 1, precio, dispo), true, true);
-	if(carritoCant.clasList.contains('carritoCountHide')){
-			carritoCant.classList.remove('carritoCountShow');
-			setTimeout(() => {carritoCant.classList.add('carritoCountShow')}, 10); 
-		}
-    };
+            const {id, precio, dispo} = prod;
+            let item = document.createElement("div");
         
-});
+            item.className = "producto";
+            item.innerHTML = `<div class="producto__titulo"><h3>${id.toUpperCase()}</h3></div>
+                            <div class="producto__img"><img src="img/${id}.jpg" alt=""></div>
+                            <div class="producto__info">
+                                <h4 class="producto__info__precio">$${precio}</h4>
+                                <input id="${id}AddBtn" class="producto__info__boton" type="button" value="">
+                             </div>;`;
+        
+            document.getElementById('productos__grid').appendChild(item);
+        
+            let addBtn = document.getElementById(id+'AddBtn');
+            addBtn.onclick = () => {
+                carrito.agregarProducto(new Producto(id, 1, precio, dispo), true, true);
+                if(carritoCant.classList.contains('carritoCountShow')){
+                    carritoCant.classList.remove('carritoCountShow');
+                    setTimeout(() => {carritoCant.classList.add('carritoCountShow')}, 10);
+                }
+            };
+                
+        });
+    })
 
-////////////////////////////////////////////Abrir y cerrar Carrito///////////////////////////////////////////////////////////////////
-let btnCarrito = document.getElementById('btnCarrito');
+
+////////////////////////////////////////////Abrir y cerrar Carrito///////////////////////////////////////////////
 let carro = document.getElementById('carrito');
 let btnCloseCarrito = document.getElementById('carrito__close__img');
 let blackOut = document.getElementById('blackOutBG');
